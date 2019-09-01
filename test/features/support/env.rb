b6f5d8c5c0de968ab1689abb25bee34668a8b116
jimjamz <jimjamz@outlook.com>
2019-09-01 23:43:23 +0100

    Initial add of travis-ci

--- test/features/support/env.rb
+++ test/features/support/env.rb
@@ -1,10 +1,11 @@
 require 'cucumber'
-require 'cucumber'
 require 'capybara'
 require 'capybara/cucumber'
+require 'capybara/poltergeist'
 require 'rspec'
 require 'selenium-webdriver'
 require 'xpath'
 
-Capybara.default_driver = :selenium
+# Capybara.default_driver = :selenium
+Capybara.default_driver = :poltergeist
 Capybara.app_host = 'http://localhost:8000/'
