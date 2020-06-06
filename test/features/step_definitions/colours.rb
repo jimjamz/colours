fff305aa1cd74396f7770a94d9e922be202ee4d5
jimjamz <jimjamz@outlook.com>
2020-06-06 18:37:21 +0100

    Implemented additional tests for custom colour scenarios

--- test/features/step_definitions/colours.rb
+++ test/features/step_definitions/colours.rb
@@ -2,22 +2,95 @@ When(/^I select the colour "(.*?)"$/) do |colour|
   click_link(colour)
 end
 
-Then(/^I am taken to the "(.*?)" page$/) do |colour|
+Then(/^I am navigated to the "(.*?)" colour page$/) do |colour|
   expect(page).to have_selector('title', text: colour, visible: false)
 end
 
-And(/^I can see the colour "(.*?)"$/) do |colour|
+And(/^I should see the colour "(.*?)"$/) do |colour|
+  def hex2rgb(in_hex)
+    # if the input is 3-digit hexadecimal notation
+    if in_hex.length < 6
+      # expand to a 6-digit hexadecimal
+      s = []
+      in_hex.split("").each do |x|
+        s.push(x.chr, x.chr)
+      end
+      in_hex = s.join("")
+    end
+    # convert each of the 3 hexadecimal pairs into a denary integer
+    t = in_hex.scan(/../).map {|color| color.to_i(16)}
+    out_rgb = "rgb(#{t[0]}, #{t[1]}, #{t[2]})"
+    return out_rgb
+  end
+
   case colour
     when 'red'
-      hex_colour = '#FF0000'
+      rgb_colour = 'rgb(255, 0, 0)'
     when 'green'
-      hex_colour = '#00FF00'
+      rgb_colour = 'rgb(0, 255, 0)'
     when 'blue'
-      hex_colour = '#0000FF'
+      rgb_colour = 'rgb(0, 0, 255)'
+    else
+      rgb_colour = hex2rgb(colour)
+      colour = 'custom'
   end
-  expect(find('#colour-row')).to have_xpath("//td[@bgcolor = '#{hex_colour}']")
+  #old:
+  # expect(find('#colour-row')).to have_xpath("//td[@bgcolor = '#{hex_colour}']")
+  # robust:
+  # expect(find('div#colours-grid')).to have_css('#selected-colour-' + colour)
+  # brittle / specific:
+  # gets the div that corresponds to the CSS class:
+  selected_colour = page.find(:css, 'div#colours-grid div#selected-colour-' + colour)
+  # Capybara reads the interpolated colour from the CSS value, 
+  # which is being rendered in HTML as an RGB value,
+  expect(selected_colour.style('background-color')).to have_content(rgb_colour)
 end
 
 And(/^I should see the text "(.*?)"$/) do |page_text|
   expect(page).to have_content(page_text)
+end
+
+Given(/^the custom colour text box is available$/) do
+  expect(page).to have_selector('input#colourpicker')
+end
+
+When(/^I enter the colour "(.*?)"/) do |hex_colour|
+  within('div#colours-selector') do
+    fill_in 'colourpicker', with: hex_colour
+  end
+end
+
+When('the custom colour is validated') do
+  find('div#colours-selector form input#colourpicker').native.send_keys(:tab)
+end
+
+Then('the valid custom colour can be submitted') do
+  within('div#colours-selector') do
+    click_button("submit")
+  end
+end
+
+Then('I should be informed that the colour is not valid') do
+  message = accept_alert do
+    find('div#colours-selector form input#colourpicker').native.send_keys(:tab)
+  end
+  expect(message).to eq('Invalid hexadecimal data.  Try again.')
+end
+
+Then('the invalid custom colour cannot be submitted') do
+  within('div#colours-selector') do
+    expect(page).to have_selector('input#submit', visible: false)
+  end
+end
+
+And(/^I should see the custom colour text "(.*?)"$/) do |custom_text|
+  within('div#colours-selector') do
+    expect(page).to have_field('colourpicker', with: custom_text)
+  end
+end
+
+And(/^I should not see the custom colour text "(.*?)"$/) do |custom_text|
+  within('div#colours-selector') do
+    expect(page).to_not have_field('colourpicker', with: custom_text)
+  end
 end
--- test/features/step_definitions/common_steps.rb
+++ test/features/step_definitions/common_steps.rb
@@ -1,3 +1,10 @@
 Given(/^I am on the home page$/) do
   visit '/index.html'
 end
+
+And(/^I should be on the "(.*?)" page$/) do | visited_page |
+  if visited_page == "home"
+    visited_page = 'index'
+  end
+  expect(page).to  have_current_path(/#{visited_page}/)
+end
