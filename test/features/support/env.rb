d2c1cfb4b7ff25ea44051c670c3f1b6a5fa719a8
jimjamz <jimjamz@outlook.com>
2020-07-14 15:47:36 +0100

    Removed Poltergeist and added Firefox and Chrome webdrivers

--- test/features/support/env.rb
+++ test/features/support/env.rb
@@ -1,14 +1,26 @@
 require 'cucumber'
 require 'capybara'
 require 'capybara/cucumber'
-require 'capybara/poltergeist'
+require 'pp'
 require 'rspec'
 require 'selenium-webdriver'
 require 'xpath'
 
-# 'selenium' as default driver for geckodriver
-# Capybara.default_driver = :selenium
-# 'poltergiest' as default driver for Travis-CI
-Capybara.default_driver = :poltergeist
-Capybara.javascript_driver = :poltergeist
+Capybara.register_driver :chrome do |app|
+  Capybara::Selenium::Driver.new app, browser: :chrome,
+    options: Selenium::WebDriver::Chrome::Options.new(args: %w[ --headless ])
+end
+
+Capybara.register_driver :firefox do |app|
+  Capybara::Selenium::Driver.new app, browser: :firefox,
+    options: Selenium::WebDriver::Firefox::Options.new(args: %w[ --headless ])
+end
+
+# Capybara::Screenshot.register_driver :$BROWSER_CHOICE do |driver, path|
+#   driver.browser.save_screenshot(path)
+# end
+
+Capybara.default_driver = :selenium
+Capybara.javascript_driver = :selenium
+
 Capybara.app_host = 'http://localhost:8080/'
