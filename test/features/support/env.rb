5914bff4e908428da9de1d3a7f3458444a88f300
jimjamz <jimjamz@outlook.com>
2020-07-15 20:32:19 +0100

    Added Capybara screenshots and Cucumber profiles for browsers and headless mode

--- test/features/support/env.rb
+++ test/features/support/env.rb
@@ -1,26 +1,37 @@
 require 'cucumber'
 require 'capybara'
 require 'capybara/cucumber'
+require 'capybara-screenshot/cucumber'
 require 'pp'
 require 'rspec'
 require 'selenium-webdriver'
 require 'xpath'
 
-Capybara.register_driver :chrome do |app|
-  Capybara::Selenium::Driver.new app, browser: :chrome,
-    options: Selenium::WebDriver::Chrome::Options.new(args: %w[ --headless ])
+case ENV['BROWSER']
+when 'firefox', 'ff'
+  BROWSER_OPTIONS = Selenium::WebDriver::Firefox::Options
+  DRIVER_CHOICE = :firefox
+when 'chrome'
+  BROWSER_OPTIONS = Selenium::WebDriver::Chrome::Options
+  DRIVER_CHOICE = :chrome
+else
+  BROWSER_OPTIONS = :null
+  DRIVER_CHOICE = :selenium
 end
 
-Capybara.register_driver :firefox do |app|
-  Capybara::Selenium::Driver.new app, browser: :firefox,
-    options: Selenium::WebDriver::Firefox::Options.new(args: %w[ --headless ])
+Capybara.register_driver ENV['BROWSER'].to_sym do |app|
+  Capybara::Selenium::Driver.new app, browser: ENV['BROWSER'].to_sym,
+    options: BROWSER_OPTIONS.new(args: [ ENV['HEADLESS_OPTION'] ])
+end
+Capybara::Screenshot.register_driver :selenium do |driver, path|
+  driver.browser.save_screenshot(path)
 end
 
-# Capybara::Screenshot.register_driver :$BROWSER_CHOICE do |driver, path|
-#   driver.browser.save_screenshot(path)
-# end
-
-Capybara.default_driver = :selenium_headless
-Capybara.javascript_driver = :selenium
+Capybara.default_driver = DRIVER_CHOICE
+Capybara.javascript_driver = DRIVER_CHOICE
 
 Capybara.app_host = 'http://localhost:8080/'
+# run another HTTP server on 3030 for screenshots that include CSS
+Capybara.asset_host = 'http://localhost:3030'
+Capybara.save_path = "screenshots"
+Capybara::Screenshot.prune_strategy = :keep_last_run
