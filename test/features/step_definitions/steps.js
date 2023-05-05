b449e0b0b65a23991d1591352f70f2f678e73f16
jimjamz <jimjamz@outlook.com>
2023-05-05 23:26:45 +0100

    Updated step definitions

--- test/features/step_definitions/steps.js
+++ test/features/step_definitions/steps.js
@@ -1,58 +1,118 @@
-const { Given, When, Then, After } = require('@cucumber/cucumber');
+// import { hex2rgb } from '../support/helpers';
+const { Given, When, Then, AfterAll } = require('@cucumber/cucumber');
 const { strict } = require('assert');
 const assert = require('assert')
-// const { colours } = require('../../..')
-const { Builder, By, until } = require('selenium-webdriver')
+const { Builder, By, Key, until } = require('selenium-webdriver')
+
+// let helpers = require('../support/helpers');
+require ("geckodriver");
+require ("chromedriver");
+
+// create web driver
+const driver = new Builder().forBrowser('firefox').build();
+
+function scan(str, regex) {
+  if (!regex.global) {
+      throw new Error("A regex without the global (/g) flag is not supported.");
+  }
+  var result = [];
+  var m;
+  while (m = regex.exec(str)) {
+      if (m.length >= 2) {
+          result.push(m.slice(1));
+      } else {
+          result.push(m[0]);
+      }
+  }
+  return result;
+};
+
+function hex2rgb(in_hex) {
+  // if the input is 3-digit hexadecimal notation
+  if (in_hex.length < 6) {
+    // expand to a 6-digit hexadecimal
+    let s = [];
+    // split the string by each character and for each ...
+    in_hex.split('').forEach(function(x) {
+      // ... push it twice onto the new array
+      s.push(x, x);
+    })
+    in_hex = s.join('');
+  }
+  
+  // convert each of the 3 hexadecimal pairs into a denary integer
+  // no scan method in Javascript like Ruby. Crafted my own.
+  let t = scan(in_hex, /../g).map(x => parseInt(x, 16));
+  // needs to be a string literal here, not a regex
+  // because the 3 hexadecimal pairs need to be interpolated.
+  const out_rgb = `rgba?\\(${t[0]}, ${t[1]}, ${t[2]}(, 1)?\\)`;
+  return out_rgb;
+};
 
 Given('I am on the home page', async function () {
-  this.driver = new Builder()
-  .forBrowser('firefox')
-  .build();
-
-  this.driver.wait(until.elementLocated(By.tagName('form')));
-  await this.driver.get('https://localhost:8080');
+  await driver.get('http://localhost:8080');
+  driver.wait(until.elementLocated(By.id('colours-selector')));
 });
 
 When('I select the colour {string}', async function (colour) {
-  //var colourCells = await this.driver.findElements(By.className('colour-cell'));
-  await this.driver.findElement(By.Id('colour-cell-' + colour)).click();
+  //var colourCells = await driver.findElements(By.className('colour-cell'));
+  await driver.findElement(By.id('colour-cell-' + colour)).click();
 });
 
-Then('I am navigated to the {string} colour page', async function (titleMatch, next) {
-  await this.driver.findElement(By.xpath('//div[contains(text(), ' + string + ')]'))
-  this.driver.getTitle()
+Then('I am navigated to the {string} colour page', async function (colour) {
+  title = await driver.getTitle()
   .then(function(title) {
-    assert.equal(title, titleMatch, next, 'Expected title to be ' + titleMatch);
-  })
+    assert.equal(title, colour);
+  });
 });
 
 Then('I should see the colour {string}', async function (colour) {
-  await this.driver.findElement(By.css('div#colours-grid div#selected-colour-' + colour))
-  .then(function(css) {
-    assert.equal(css, colour);
-  })
+  switch (colour) {
+    case 'red':
+      rgb_colour = /rgba?\(255, 0, 0(, 1)?\)/
+      break;
+    case 'green':
+      rgb_colour = /rgba?\(0, 255, 0(, 1)?\)/
+      break;
+    case 'blue':
+      rgb_colour = /rgba?\(0, 0, 255(, 1)?\)/
+      break;
+    default:
+      //rgb_colour is still a string literal, not a regular expression
+      //needs to be converted to be a matcher for assertions
+      rgb_colour = new RegExp(hex2rgb(colour));
+      colour = 'custom';
+      break;
+  }
+
+  let selected_colour = await driver.findElement(By.id('selected-colour-' + colour)).getCssValue("background-color")
+  .then(function(selected_colour) {
+    assert(selected_colour.match(rgb_colour));
+  });
 });
 
-Then(/^'I should see the text {string}'$/, function (page_text) {
-  assert.equal(strict, page_text)
+Then(/^I should see the text "(.*?)"$/, async function (expected_text) {
+  let page_text = await driver.findElement(By.tagName('p')).getText();
+  assert.equal(expected_text + ".", page_text)
 });
 
-Given(/^(?:'the custom colour text box is available'|'custom colours can be created)'$/, async function () {
-  await this.driver.findElement(By.Id('colourpicker'));
+Given(/^(?:the custom colour text box is available|custom colours can be created)$/, async function () {
+  await driver.findElement(By.id('colourpicker'));
 });
 
-When(/^I (?:'enter|try to create) the(?: custom)? colour {string}'$/, async function (colour) {
-    await this.driver.findElement(By.Id('colourpicker')).clear().sendKeys(colour);
-  end
-end
+When(/^I (?:enter|try to create) the(?: custom)? colour "(.*?)"/, async function (colour) {
+//When(/^I (?:enter|try to create) the colour "(.*?)"/, async function (colour) {
+await driver.findElement(By.id('colourpicker')).sendKeys(colour);
 });
-// When('the custom colour is validated') do
-//   pending # Write code here that turns the phrase above into concrete actions
-// end
 
-// Then('the valid custom colour can be submitted') do
-//   pending # Write code here that turns the phrase above into concrete actions
-// end
+When('the custom colour is validated', async function () {
+  // await driver.findElement(By.id('colourpicker')).sendKeys('\uE004');
+  await driver.findElement(By.id('colourpicker')).sendKeys(Key.TAB);
+});
+
+Then('the valid custom colour can be submitted', async function () {
+  await driver.findElement(By.id('submit')).click();
+});
 
 // Given('custom colours can be created') do
 //   pending # Write code here that turns the phrase above into concrete actions
@@ -90,7 +150,6 @@ end
 //   pending # Write code here that turns the phrase above into concrete actions
 // end
 
-After(async function () {
-  this.driver.close();
+AfterAll(async function(){
+  await driver.quit();
 });
-})
